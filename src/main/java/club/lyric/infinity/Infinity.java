bd6058553ba9bd28e2276c81e47fc3c34b439afc
lyricnight <79311706+lyricnight@users.noreply.github.com>
2023-12-11 21:05:57 +0000

    added bush eventbus dependency - this probably won't work because its meant for java 8

--- src/main/java/club/lyric/infinity/Infinity.java
+++ src/main/java/club/lyric/infinity/Infinity.java
@@ -1,5 +1,7 @@
 package club.lyric.infinity;
 
+import me.bush.eventbus.bus.EventBus;
+import me.bush.eventbus.handler.handlers.LambdaHandler;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 
@@ -9,6 +11,8 @@ import org.slf4j.LoggerFactory;
 
 public class Infinity implements ModInitializer, ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("infinity");
+
+	public static EventBus EVENT_BUS = new EventBus(LambdaHandler.class, LOGGER::error, LOGGER::info);
 
 	@Override
 	public void onInitialize() {
