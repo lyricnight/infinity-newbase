d91511a644ee8f3e7f770b8e48f8fec5ee2913dd
legendaryhvher <141691619+legendaryhvher@users.noreply.github.com>
2023-12-13 12:47:46 -0800

    Render2DUtils

--- src/main/java/club/lyric/infinity/api/util/render/Render2DUtils.java
+++ src/main/java/club/lyric/infinity/api/util/render/Render2DUtils.java
@@ -1,4 +1,42 @@
 package club.lyric.infinity.api.util.render;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.math.MatrixStack;
+import org.joml.Matrix4f;
+
 public class Render2DUtils {
+
+    public static void drawRect(MatrixStack matrices, float x, float y, float width, float height, int color) {
+        Matrix4f matrix = matrices.peek().getPositionMatrix();
+
+        // Colors
+        float r = (float) (color >> 16 & 255) / 255.0F;
+        float g = (float) (color >> 8 & 255) / 255.0F;
+        float b = (float) (color & 255) / 255.0F;
+        float a = (float) (color >> 24 & 255) / 255.0F;
+
+        // Rect starts here
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        setup();
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+        bufferBuilder.vertex(matrix, x, y + height, 0.0F).color(r, g, b, a).next();
+        bufferBuilder.vertex(matrix, x + width, y + height, 0.0F).color(r, g, b, a).next();
+        bufferBuilder.vertex(matrix, x + width, y, 0.0F).color(r, g, b, a).next();
+        bufferBuilder.vertex(matrix, x, y, 0.0F).color(r, g, b, a).next();
+        Tessellator.getInstance().draw();
+        end();
+        // Rect ends here
+    }
+
+    public static void setup() {
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+    }
+
+    public static void end() {
+        RenderSystem.disableBlend();
+    }
+
 }
--- src/main/java/club/lyric/infinity/asm/MixinTitleScreen.java
+++ src/main/java/club/lyric/infinity/asm/MixinTitleScreen.java
@@ -1,5 +1,6 @@
 package club.lyric.infinity.asm;
 
+import club.lyric.infinity.api.util.render.Render2DUtils;
 import club.lyric.infinity.api.util.render.text.TextUtils;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.TitleScreen;
@@ -16,6 +17,9 @@ public abstract class MixinTitleScreen {
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)I", ordinal = 0))
     private void onRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+
         TextUtils.drawStringWithShadow(context, mc.textRenderer, Text.of("t.me/dotgod.cc"), 2, 2, -1);
+
+        Render2DUtils.drawRect(context.getMatrices(), 20, 20, 20, 20, -1);
     }
 }
