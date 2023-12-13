be3907c4a2feafbed3716f839e55c576a098ec18
lyricnight <79311706+lyricnight@users.noreply.github.com>
2023-12-13 20:27:41 +0000

    Revert "vasler at least learn how to paste"
    This reverts commit 370f9d76830f678e20b0852c08011d5255650d65.

--- src/main/java/club/lyric/infinity/asm/MixinTitleScreen.java
+++ src/main/java/club/lyric/infinity/asm/MixinTitleScreen.java
@@ -1,6 +1,5 @@
 package club.lyric.infinity.asm;
 
-import club.lyric.infinity.api.util.minecraft.IMinecraft;
 import club.lyric.infinity.api.util.text.TextUtils;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.TitleScreen;
@@ -10,8 +9,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = TitleScreen.class)
-public class MixinTitleScreen implements IMinecraft {
+import static club.lyric.infinity.api.util.minecraft.IMinecraft.mc;
+
+@Mixin(value={TitleScreen.class})
+public abstract class MixinTitleScreen {
+
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)I", ordinal = 0))
     private void onRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         TextUtils.drawStringWithShadow(mc.textRenderer, Text.of("t.me/dotgod.cc"), 2, 2, -1);
