2566b45af028d17dac2e462dab71fbb5648dec8d
lyricnight <79311706+lyricnight@users.noreply.github.com>
2023-12-28 20:40:03 +0000

    fov mixin

---
+++ src/main/java/club/lyric/infinity/asm/MixinMinecraftOptions.java
@@ -0,0 +1,14 @@
+package club.lyric.infinity.asm;
+
+import net.minecraft.client.option.GameOptions;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(GameOptions.class)
+public class MixinMinecraftOptions {
+    @ModifyConstant(method = "<init>", constant = @Constant(intValue = 110))
+    private int maxFovValueHook(int constant) {
+        return 180;
+    }
+}
